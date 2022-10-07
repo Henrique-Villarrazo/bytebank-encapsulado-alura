077ae76e255909fbc4cf39ed94e21734da4fbbb3
Henrique <henrique.villarrazo@gmail.com>
2022-10-07 17:49:31 -0300

    Adicionando projetos base

---
+++ src/TestaGetSet.java
@@ -0,0 +1,25 @@
+
+public class TestaGetSet {
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337, 24226);
+		conta.setNumero(1337);
+		System.out.println(conta.getNumero());	
+		
+		Cliente paulo = new Cliente();
+		
+		paulo.setNome("Paulo Silveira");
+		
+		conta.setTitular(paulo);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programador");  
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
+	}
+}
